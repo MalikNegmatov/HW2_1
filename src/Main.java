88ce43fe20094ee5affaa93bc7c66443c176e4ac
MalikNegmatov <maliknegmatov@gmail.com>
2023-06-09 14:49:42 +0300

    Выполнили условия задачи 2.9.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
 
     public static void main(String[] args) {
+        Person person = new Person("Lyapis", "Trubetskoy");
+        System.out.println(person);
+        changePerson(person);
+        System.out.println(person);
+    }
+    public static void changePerson(Person person) {
+        person = new Person("Ilya", "Lagutenko");
+        System.out.println("в методе - " + person);
     }
 }
